394f9ed68af314524318fd0ce73f318356b8a4b9
vovo255 <33264648+vovo255@users.noreply.github.com>
2023-08-26 14:36:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
-sudo apt-get install libssl-dev libusb-1.0-0-dev libudev-dev pkg-config libgtk-3-dev
-sudo apt-get install git wget cmake build-essential
-sudo apt-get install libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev at
 sudo apt install curl
 sudo mkdir -p /etc/apt/keyrings
 curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
