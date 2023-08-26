02ba4dbd4b474ba7b0fd165b00f02f2b5c50f8ef
vovo255 <33264648+vovo255@users.noreply.github.com>
2023-08-26 14:17:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 sudo apt-get install libssl-dev libusb-1.0-0-dev libudev-dev pkg-config libgtk-3-dev
 sudo apt-get install git wget cmake build-essential
 sudo apt-get install libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev at
+sudo apt install curl
 sudo mkdir -p /etc/apt/keyrings
 curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
@@ -10,7 +11,7 @@ sudo apt-get update
 sudo apt-get install librealsense2-dkms
 sudo apt-get install librealsense2-utils
 sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
-sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
 sudo apt update
 sudo apt install anydesk
-sudo echo "waybot123" | anydesk --set-password
+echo "waybot123" | sudo anydesk --set-password
