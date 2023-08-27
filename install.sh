b60df55c60e222042f5783e442e0f04383d1dde6
vovo255 <33264648+vovo255@users.noreply.github.com>
2023-08-27 18:15:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+source /home/waybot/.bashrc
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+sudo apt-get install libssl-dev libusb-1.0-0-dev libudev-dev pkg-config libgtk-3-dev
+sudo apt-get install git wget cmake build-essential
+sudo apt-get install libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev at
 sudo apt install curl
 sudo mkdir -p /etc/apt/keyrings
 curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
@@ -15,6 +20,14 @@ echo "waybot123" | sudo anydesk --set-password
 sudo apt install python3-pip
 pip3 install pyrealsense2
 pip3 install pyserial
-sudo apt install python3-pyqt5
 sudo apt-get install python3-opencv
 sudo apt install qtwebengine5-dev
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install segmentation-models-pytorch
+pip3 install PyQtWebEngine
+pip3 install ultralytics
+pip3 install PyQt5
+anydesk --get-id
+pip3 install numpy
+sudo apt install libzbar0
+pip3 install pyzbar
