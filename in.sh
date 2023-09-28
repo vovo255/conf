5dc66667ee4f037f39f0ab193d145492cc1677f2
vovo255 <33264648+vovo255@users.noreply.github.com>
2023-09-28 18:15:10 +0300

    Create in.sh

---
+++ in.sh
@@ -0,0 +1,14 @@
+pip3 install imutils
+
+
+sudo apt install v4l-utils
+
+pip3 uninstall opencv-python
+
+pip3 install opencv-python-headless
+
+sudo apt remove brltty
+
+sudo usermod -a -G tty waybot
+
+sudo usermod -a -G dialout waybot
